52ca42efdadc97e87081f9f5db8030cbe6f7d5ca
Thijs Elferink <thijs.elferink@topicus.nl>
2016-01-15 15:18:03 +0100

    env parameters gebruiken

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,13 @@
+#check env
+: ${DOMAIN?		"You need to set environment variable DOMAIN"}
+: ${USERNAME?	"You need to set environment variable USERNAME"}
+: ${PASSWORD?	"You need to set environment variable PASSWORD"}
+: ${URL?		"You need to set environment variable URL"}
+: ${ROOMS?		"You need to set environment variable ROOMS"}
+
 cd /tmp
 rm -rf topical-backend; true
 git clone https://github.com/niesink/topical-backend.git
 cd topical-backend
 mvn compile
-mvn exec:java -Dexec.mainClass="nl.topicus.topical.TopicalBackend" -Dexec.args="[domain] [username] [password] [url] [room1] [room2]"
+mvn exec:java -Dexec.mainClass="nl.topicus.topical.TopicalBackend" -Dexec.args="$DOMAIN $USERNAME $PASSWORD $URL $ROOMS"
